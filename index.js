9e1cd0ae5cd3e788b5044e21f2037fd2d56cd268
rcluisdaniel <rcluisdaniel@gmail.com>
2020-06-18 00:21:55 -0500

    2 files modified

--- index.js
+++ index.js
@@ -3,16 +3,38 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-function Componente(){
-  return <p>Puto componente</p>
-}
+class Blog extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      articles: []
+    }    
+  }
 
-class ComponenteDeClase extends Component{
+  componentDidMount(){
+    let promesa = fetch('https://jsonplaceholder.typicode.com/posts')
+
+    promesa.then(response => response.json()).then(data=>{
+      console.log(data)
+      this.setState({
+        articles: data
+      })
+      
+    })
+  }
+
   render(){
-    return <p>El puto componente ahora es de clase</p>
+    return(
+      <div>
+        {
+          this.state.articles.map((article)=>{
+            return <div classname="card" style ={backgroundColor:'red',color:'yelo'}><p>{article.title}</p></div>
+          })
+        }
+      </div>
+    )
   }
 }
-
 
 class App extends Component {
   constructor() {
@@ -25,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ComponenteDeClase/>
+        <Blog/>
       </div>
     );
   }
